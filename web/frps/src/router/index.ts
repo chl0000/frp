17982a91efff4fcde704eac0711628f6656c3c67
chl0000 <shdzhqxzh@163.com>
2025-12-31 16:38:51 +0800

    Add files via upload

--- web/frps/src/router/index.ts
+++ web/frps/src/router/index.ts
@@ -7,6 +7,8 @@ import ProxiesHTTPS from '../components/ProxiesHTTPS.vue'
 import ProxiesTCPMux from '../components/ProxiesTCPMux.vue'
 import ProxiesSTCP from '../components/ProxiesSTCP.vue'
 import ProxiesSUDP from '../components/ProxiesSUDP.vue'
+import ProxiesXTCP from '../components/ProxiesXTCP.vue'
+import ClientList from '../components/ClientList.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -50,6 +52,16 @@ const router = createRouter({
       path: '/proxies/sudp',
       name: 'ProxiesSUDP',
       component: ProxiesSUDP,
+    },
+    {
+      path: '/proxies/xtcp',
+      name: 'ProxiesXTCP',
+      component: ProxiesXTCP,
+    },
+    {
+      path: '/client/list',
+      name: 'ClientList',
+      component: ClientList,
     },
   ],
 })
